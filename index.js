6ab8f8e2eca8286ba9cb1465a40ab2bd45429bec
Dave Banwell <dave@davebanwell.com>
2024-04-25 22:16:53 -0400

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouNotes = [];
+    for(let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        thankYouNotes.splice(i, 0, message);
+    }
+    return thankYouNotes;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
